5f98f7a18cc23f7fdad6943b5c05454432047df0
Anil kumar nayak <anilkumarnayak639@gmail.com>
2023-10-31 17:04:03 +0530

    Login page with fireBase

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./Pages/Home";
 import Login from "./Pages/Login";
 import SignUp from "./Pages/SignUp";
+import Error from "./Components/Error";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+const auth = getAuth();
 function App() {
+  //to check user authentication status
+  const [user, setUser] = useState(null);
+
+  // for keep track of users status
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route
+          path="/home"
+          element={user ? <Home /> : <Navigate to="/login" />}
+        />
         <Route path="/signUp" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </div>
   );
--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,7 +1,100 @@
-import React from "react";
+import React, { useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebaseConfig";
+import {
+  Card,
+  CardContent,
+  Typography,
+  TextField,
+  Button,
+} from "@mui/material";
+import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
-  return <div>Login</div>;
+  const navigate = useNavigate();
+
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [errorMessage, setErrorMessage] = useState("");
+  //check user mail and password in firebase
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, values.email, values.password)
+      .then((res) => {
+        navigate("/home");
+      })
+      .catch((err) => {
+        setErrorMessage(err.message); //for error messages
+      });
+  };
+  return (
+    <div style={{ backgroundColor: "#70C0F7" }}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          minHeight: "100vh",
+        }}
+      >
+        <Card sx={{ width: "400px", height: "500px", margin: "10px" }}>
+          <CardContent>
+            <Typography variant="h5" component="div">
+              Create An Account
+            </Typography>
+            <form onSubmit={handleSubmit}>
+              <TextField
+                name="email"
+                label="Email"
+                type="email"
+                variant="outlined"
+                fullWidth
+                margin="normal"
+                required
+                onChange={(e) =>
+                  setValues((data) => ({ ...data, email: e.target.value }))
+                }
+              />
+              <TextField
+                name="password"
+                label="Password"
+                type="password"
+                variant="outlined"
+                fullWidth
+                margin="normal"
+                required
+                onChange={(e) =>
+                  setValues((data) => ({ ...data, password: e.target.value }))
+                }
+              />
+              <Typography sx={{ color: "red" }}>{errorMessage}</Typography>
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                fullWidth
+                size="large"
+                sx={{ marginTop: "25px" }}
+              >
+                Login
+              </Button>
+              <Typography mt={5}>
+                Don't Have An Account?
+                <span>
+                  <Link to="/signup" style={{ textDecoration: "none" }}>
+                    SignUp
+                  </Link>
+                </span>
+              </Typography>
+            </form>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
